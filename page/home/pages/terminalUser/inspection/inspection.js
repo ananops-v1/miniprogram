47e039bc7a62dce41085d8c15e9f88d7ddb3242d
SongOf <lisongsong2015@bupt.edu.cn>
2020-04-07 20:58:45 +0800

    去掉网点

--- page/home/pages/terminalUser/inspection/inspection.js
+++ page/home/pages/terminalUser/inspection/inspection.js
@@ -100,7 +100,7 @@ Page({
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       providerIndex: e.detail.value,
-      providerId: programList[e.detail.value].partyBId
+      providerId: this.data.programList[e.detail.value].partyBId
     })
   },
   //选择项目事件
